b48888999172c5a542e565258b46a448a6592609
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-16 22:17:48 +0000

    customise product and category displays in admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,22 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
