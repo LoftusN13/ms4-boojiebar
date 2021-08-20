01a42694ab6c95977d07235e7a2d0184f2165826
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-20 22:51:46 +0000

    Update main nav urls and products views.py for sorting by name, price and rating

--- products/views.py
+++ products/views.py
@@ -13,13 +13,30 @@ def all_products(request):
     includes filtering products by
     users search queries.
     includes filtering products by
-    category.
+    category. includes sorting products
+    by name, rating and price.
     """
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if 'sortkey' == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -36,10 +53,13 @@ def all_products(request):
                 name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
