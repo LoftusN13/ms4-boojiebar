fab7c6353b92a14be2aebfb6271a1182c889e7ba
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-23 22:42:42 +0000

    Add messages in views for removing and updating products from shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import (
-    render, redirect, reverse, HttpResponse)
+    render, redirect, reverse, HttpResponse,
+    get_object_or_404)
 from django.contrib import messages
 from products.models import Product
 
@@ -20,6 +21,8 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}!')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag!')
@@ -31,14 +34,18 @@ def add_to_bag(request, item_id):
 def edit_bag(request, item_id):
     # edit quantity of specified product in bag
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}!')
 
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag!')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -48,13 +55,16 @@ def delete_from_bag(request, item_id):
     # delete specified product from bag
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         if item_id in bag:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag!')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
