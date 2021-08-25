1f6d2979c549264fa9997b7833e5f1b0a1d43982
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-25 15:27:38 +0000

    Add Stripe secret and public keys to checkout views

--- boojie_bar/settings.py
+++ boojie_bar/settings.py
@@ -159,6 +159,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
 
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
