0b05424a0a6703aa27e8769a52fc792a2f37b9e0
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-30 13:23:37 +0000

    Add funtionality to edit_product view

--- products/views.py
+++ products/views.py
@@ -103,8 +103,18 @@ def add_product(request):
 def edit_product(request, product_id):
     # Edits specific product in the store
     product = get_object_or_404(Product, pk=product_id)
-    form = ProductForm(instance=product)
-    messages.info(request, f'You are editing {product.name}')
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_details', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. \
+                Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
 
     template = 'products/edit_product.html'
     context = {
