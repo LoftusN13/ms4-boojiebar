848b5acc3535de87052b9db40ec9faea13e5799e
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-30 13:12:38 +0000

    Add edit_product url, template and view

--- products/views.py
+++ products/views.py
@@ -98,3 +98,18 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    # Edits specific product in the store
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
