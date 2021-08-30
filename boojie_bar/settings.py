25afda1b0c8646d6c67a17f0e03a88b031b55f2d
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-30 16:12:59 +0000

    Setup for deployment to heroku

--- boojie_bar/settings.py
+++ boojie_bar/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +114,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
