c00468c55895b69b5cb0be95eb1232de1b7826a7
LoftusN13 <loftus.niamh@yahoo.ie>
2021-08-30 16:37:51 +0000

    Add Procfile and update allowed hosts in settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boojie_bar.wsgi:application
--- boojie_bar/settings.py
+++ boojie_bar/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-u$w7uu@6ha0#ih9*=v88)95pt)l*ys64mtxs74s&#dk(7tc9a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-boojiebar.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -108,12 +108,17 @@ WSGI_APPLICATION = 'boojie_bar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
