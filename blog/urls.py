5c7a4885fc64ca8ca71839761c4eeb2d7378cdc6
LoftusN13 <loftus.niamh@yahoo.ie>
2021-09-01 20:10:57 +0000

    Add url, view and template to edit_blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_blog_posts, name='blogs'),
     path('<int:blog_id>/', views.blog_details, name='blog_details'),
     path('add/', views.add_blog, name='add_blog'),
+    path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -53,3 +53,29 @@ def add_blog(request):
     }
 
     return render(request, template, context)
+
+
+def edit_blog(request, blog_id):
+    # view to edit specific blog post
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog post!')
+            return redirect(reverse('blog_details', args=[blog.id]))
+        else:
+            messages.error(request, 'Failed to update blog post. \
+                Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {blog.title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+
+    return render(request, template, context)
